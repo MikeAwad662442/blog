5a851ae0ebaa25048c99c613101ba80b96fa29aa
Mike Awad <mikeawad662442@gmail.com>
2024-04-08 20:00:08 +0300

    start project

---
+++ prisma/README.md
@@ -0,0 +1,29 @@
+---
+author: "Mike Awad"
+---
+
+# Prisma siting
+
+to generate Prisma client
+
+```js
+    npx prisma init --datasource-provider sqlite
+```
+
+to build a tables in database
+
+```js
+     npx prisma db push
+```
+
+to generate tables
+
+```js
+    npx prisma generate
+```
+
+prisma studio ++localhost:5555++
+
+```js
+    npx prisma studio
+```
---
+++ src/lib/prisma.ts
@@ -0,0 +1,16 @@
+//  Use this Code to prevent Next.js from create new client every time you restart the app
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
