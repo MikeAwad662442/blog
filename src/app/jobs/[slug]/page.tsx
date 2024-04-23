9f31b12504644ba2ae0fa49064587837d2e132bd
Mike Awad <mikeawad662442@gmail.com>
2024-04-23 15:03:22 +0300

    Revert "generate Static Params"
    This reverts commit 5bde35d3562ccf3dee64c130153893cf570e53fa.

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,6 +1,5 @@
 // create dynamic page
 import JobPage from "@/components/JobPage";
-import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
@@ -10,17 +9,6 @@ import { cache } from "react";
 interface PageProps {
   params: { slug: string };
 }
-// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-// it helps to make a page faster by controlling cash pages statically because the "slug" page runs as dynamic (server rendered) so to make it statically we use this function
-export const generateStaticParams = async () => {
-  const jobs = await prisma.job.findMany({
-    where: { approved: true },
-    select: { slug: true },
-  });
-
-  return jobs.map(({ slug }) => slug);
-};
-// 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 // We use const to get the value from the database once even if we use it in two different places
 const getJob = cache(async (slug: string) => {
   const job = await prisma.job.findUnique({
@@ -41,26 +29,15 @@ export const generateMetadata = async ({
 };
 const page = async ({ params: { slug } }: PageProps) => {
   const job = await getJob(slug);
-
-  const { applicationEmail, applicationUrl } = job;
-
-  const applicationLink = applicationEmail
-    ? `mailto:${applicationEmail}`
-    : applicationUrl;
-
-  if (!applicationLink) {
-    console.error("Job has no application link or email");
-    notFound();
-  }
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobPage job={job} />
       <aside>
-        <Button asChild>
-          <a href={applicationLink} className="w-40 md:w-fit">
-            Apply now
-          </a>
-        </Button>
+        {/* <Button asChild>
+            <a href={applicationLink} className="w-40 md:w-fit">
+              Apply now
+            </a>
+          </Button> */}
       </aside>
     </main>
   );
